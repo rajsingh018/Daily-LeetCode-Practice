628c96a3084d6ca0050c9186bf7e0786ec2288f3
Madhvendra Pratap Singh <83414416+rajsingh018@users.noreply.github.com>
2023-07-18 14:02:01 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (85.94%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -10,16 +10,20 @@
  * };
  */
 class Solution {
+private:
+    void inorder(TreeNode* root, vector<int> &ans){
+        if(!root) return;
+        
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right, ans);
+    }
+    
 public:
     
-    vector<int>v;
     vector<int> inorderTraversal(TreeNode* root) {
-        if(root!=NULL){
-            inorderTraversal(root->left);
-            v.push_back(root->val);
-            inorderTraversal(root->right);
-        }
-        return v;
-        
+        vector<int> ans;
+        inorder(root, ans);
+        return ans;
     }
 };
