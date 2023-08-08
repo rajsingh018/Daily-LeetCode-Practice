aae62a06a6782df1baa23b0e8b2781ea8ac13045
Madhvendra Pratap Singh <83414416+rajsingh018@users.noreply.github.com>
2023-08-08 23:59:03 +0530

    Time: 14 ms (88.04%), Space: 19.7 MB (14.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()/2];
+        // Moore's Algorithm
+            int count=0, ele=0;
+            for(int i=0; i<nums.size(); i++){
+                if(count==0){
+                    ele=nums[i];
+                    count++;
+                }
+                else{
+                    if(ele==nums[i]){
+                        count++;
+                    }
+                    else{
+                        count--;
+                    }
+                }
+            }
+            return ele;
     }
 };
