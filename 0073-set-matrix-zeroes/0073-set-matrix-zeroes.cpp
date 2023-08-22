9697f0f41d120d2ae433bd33e8887d4f364a130c
Madhvendra Pratap Singh <83414416+rajsingh018@users.noreply.github.com>
2023-08-22 19:48:29 +0530

    Time: 12 ms (77.07%), Space: 13.6 MB (8.11%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& v) {
-        vector<int>k1;
-        vector<int>k2;
-        for(int i=0;i<v.size();i++){
-            for(int j=0;j<v[0].size();j++){
-                if(v[i][j]==0){
-                    k1.push_back(i);
-                    k2.push_back(j);
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<int> k(n,0);
+        vector<int> l(m,0);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]==0){
+                    k[i]=1;
+                    l[j]=1;
                 }
             }
         }
-        for(int i=0;i<k1.size();i++){
-            int p=k1[i];
-            for(int j=0;j<v[0].size();j++){
-                v[p][j]=0;
-            }
-        }
-        for(int i=0;i<k2.size();i++){
-            int p=k2[i];
-            for(int j=0;j<v.size();j++){
-                v[j][p]=0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(k[i]==1 or l[j]==1){
+                    matrix[i][j]=0;
+                }
             }
         }
         
-        for(int i=0;i<v.size();i++){
-            for(int j=0;j<v[0].size();j++){
-                cout<<v[i][j]<<" ";
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cout<<matrix[i][j]<<" ";
             }
             cout<<endl;
         }
