77d2fdaca1b0782c4aa55d366ee7b506381de958
Madhvendra Pratap Singh <83414416+rajsingh018@users.noreply.github.com>
2023-08-28 22:47:21 +0530

    Time: 11 ms (69.83%), Space: 16 MB (28.84%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
-    
-    vector<int> majorityElement(vector<int>& v) {
-	// Write your code here
-	int n= v.size();
-	map<int, int> mp;
-	for(auto x:v){
-		mp[x]++;
-	}
-	vector<int> ans;
-	for(auto x:mp){
-		if(x.second>floor(n/3)){
-			ans.push_back(x.first);
-		}
-	}
-	return ans;
-}
+    vector<int> majorityElement(vector<int>& nums) {
+        int count1=0, count2=0;
+        int ele1 = INT_MIN, ele2 = INT_MIN;
+        
+        for(int i=0; i<nums.size(); i++){
+            if(count1==0 and ele2!=nums[i]){
+                count1=1;
+                ele1=nums[i];
+            }
+            else if(count2==0 and ele1!=nums[i]){
+                count2=1;
+                ele2=nums[i];
+            }
+            else if(nums[i]==ele1) count1++;
+            else if(nums[i]==ele2) count2++;
+            else {
+                count1--;
+                count2--;
+            }
+        }
+        vector<int> ans;
+        count1=0; count2=0;
+        for(int i=0; i<nums.size();i++){
+            if(nums[i]==ele1) count1++;
+            else if(nums[i]==ele2) count2++;
+        }
+        if(count1>floor(nums.size()/3)) ans.push_back(ele1);
+        if(count2>floor(nums.size()/3)) ans.push_back(ele2);
+        
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
 };
